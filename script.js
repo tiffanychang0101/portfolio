18d05b0ae60b76f2ab9d4f2c086ee8bd1957424d
tiffanychang0101 <tiffanywhither@gmail.com>
2025-02-03 17:28:21 +0800

    測試套版

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-// ------- Osmo [https://osmo.supply/] ------- //
+const allLinks = document.querySelectorAll("a");
 
-document.addEventListener("DOMContentLoaded", () => {
-	// Register GSAP Plugins
-  gsap.registerPlugin(ScrollTrigger);
-  // Parallax Layers
-  document.querySelectorAll('[data-parallax-layers]').forEach((triggerElement) => {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: triggerElement,
-        start: "0% 0%",
-        end: "100% 0%",
-        scrub: 0
-      }
-    });
-    const layers = [
-      { layer: "1", yPercent: 70 },
-      { layer: "2", yPercent: 55 },
-      { layer: "3", yPercent: 40 },
-      { layer: "4", yPercent: 10 }
-    ];
-    layers.forEach((layerObj, idx) => {
-      tl.to(
-        triggerElement.querySelectorAll(`[data-parallax-layer="${layerObj.layer}"]`),
-        {
-          yPercent: layerObj.yPercent,
-          ease: "none"
-        },
-        idx === 0 ? undefined : "<"
-      );
-    });
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    if (!document.startViewTransition) {
+      setActiveItem(event.target);
+      return;
+    }
+
+    document.startViewTransition(() => setActiveItem(event.target));
   });
 });
-/* Lenis */
-const lenis = new Lenis();
-lenis.on('scroll', ScrollTrigger.update);
-gsap.ticker.add((time) => {lenis.raf(time * 1000);});
-gsap.ticker.lagSmoothing(0);
+
+function setActiveItem(element) {
+  allLinks.forEach((link) => link.classList.remove("active"));
+  element.classList.add("active");
+}
